3884cbd1c32fdb13ea14af1990cdc50226ec56b0
zhaoyanjun <362299465@qq.com>
2016-09-27 18:04:43 +0800

    update imageloader version to 1.3.0

--- app/src/main/java/com/zyj/app/MainActivity.java
+++ app/src/main/java/com/zyj/app/MainActivity.java
@@ -1,18 +1,14 @@
 package com.zyj.app;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.zyj.app.imageload.load.HttpLoader;
-import com.zyj.app.imageload.util.MyTask;
+import com.zyj.app.imageload.ImageLoad;
 import com.zyj.app.util.Contans;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,37 +30,7 @@ public class MainActivity extends AppCompatActivity {
         }); ;
 
         imageView = (ImageView) findViewById( R.id.image);
-
-        new MyTask<>().setTaskListener(new MyTask.TaskListener() {
-            @Override
-            public void start() {
-
-            }
-
-            @Override
-            public void update(Object o) {
-
-            }
-
-            @Override
-            public Object doInBackground(Object o) {
-                return  HttpLoader.load( Contans.imageUrls[0] ) ;
-            }
-
-            @Override
-            public void result(Object o) {
-                Bitmap bitmap = (Bitmap) o;
-                if ( bitmap != null ){
-                    imageView.setImageBitmap((Bitmap) o) ;
-                    Toast.makeText(MainActivity.this, "dddd", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        }).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR , "" ) ;
-
-     //   imageView.setImageBitmap( new HttpLoader(Contans.imageUrls[0] ).load());
-
-       // ImageLoad.get( MainActivity.this ).load(  Contans.imageUrls[0], imageView );
+        ImageLoad.get( MainActivity.this ).load(  Contans.imageUrls[0], imageView );
 
         mrecyclerView = (RecyclerView)findViewById(R.id.recycleview);
         mrecyclerView.setLayoutManager(new GridLayoutManager( MainActivity.this , 2)) ;   //gridView网格布局
